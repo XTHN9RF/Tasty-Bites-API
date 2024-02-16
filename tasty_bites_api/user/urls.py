9d85801c5d687b5e9919b7eb33ba9dc58b484e83
Vladyslav Duran <justez4rtz@gmail.com>
2024-02-16 22:13:09 +0200

    feat(user_urls): add registration,auth and password reset urls

--- tasty_bites_api/user/urls.py
+++ tasty_bites_api/user/urls.py
@@ -1,4 +1,13 @@
-from django.urls import path
+from django.urls import (path,
+                         include)
+
+from dj_rest_auth import views as dj_rest_auth_views
+
+namespace = 'user'
 
 urlpatterns = [
+    path('registration/', include('dj_rest_auth.registration.urls')),
+    path('auth/', include('dj_rest_auth.urls')),
+    path('password/reset/confirm/<uidb64>/<token>/', dj_rest_auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
 ]
