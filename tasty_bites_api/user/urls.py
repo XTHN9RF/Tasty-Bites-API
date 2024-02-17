5bed9e1dc729999e7e4f7e8d9d04964f6bf76c50
Vladyslav Duran <justez4rtz@gmail.com>
2024-02-17 19:46:26 +0200

    feat(auth): create views for registration and login process and add them to api urls, add refresh token url to it likewise

--- tasty_bites_api/user/urls.py
+++ tasty_bites_api/user/urls.py
@@ -1,7 +1,13 @@
-from django.urls import (path,
-                         include)
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from .views import UserRegistrationView
+from .views import UserLoginView
+
 namespace = 'user'
 
 urlpatterns = [
-
+    path('/register/', UserRegistrationView.as_view(), name='register'),
+    path('/login/', UserLoginView.as_view(), name='login'),
+    path('/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- tasty_bites_api/user/views.py
+++ tasty_bites_api/user/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import generics
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from .models import User
+from .serializers import UserRegistrationSerializer
+from .serializers import UserLoginSerializer
+
+
+class UserLoginView(TokenObtainPairView):
+    permission_classes = (AllowAny,)
+    serializer_class = UserLoginSerializer
+
+
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer
+    permission_classes = (AllowAny,)
