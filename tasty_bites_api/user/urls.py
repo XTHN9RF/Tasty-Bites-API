e1409d7d300593acc0ab52067043de5be123e934
Vladyslav Duran <justez4rtz@gmail.com>
2024-02-17 18:18:02 +0200

    refactor(auth): remove all codelines connected to old auth system

--- tasty_bites_api/user/urls.py
+++ tasty_bites_api/user/urls.py
@@ -1,13 +1,7 @@
 from django.urls import (path,
                          include)
-
-from dj_rest_auth import views as dj_rest_auth_views
-
 namespace = 'user'
 
 urlpatterns = [
-    path('registration/', include('dj_rest_auth.registration.urls')),
-    path('auth/', include('dj_rest_auth.urls')),
-    path('password/reset/confirm/<uidb64>/<token>/', dj_rest_auth_views.PasswordResetConfirmView.as_view(),
-         name='password_reset_confirm'),
+
 ]
