69d8ef7dfe99508e3c03582658c138b553651a81
Vladyslav Duran <justez4rtz@gmail.com>
2024-02-17 18:17:08 +0200

    refactor(validators): remove self parameter from all validators and change self calls to User model calls

--- tasty_bites_api/user/validators.py
+++ tasty_bites_api/user/validators.py
@@ -1,25 +1,28 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
 
+User = get_user_model()
 
-def user_creation_validator(self, username: str, email: str, password: str, confirm_password) -> bool:
+
+def user_creation_validator(username: str, email: str, password: str, confirm_password) -> bool:
     """Validate the user data before saving it to the database."""
     try:
         validate_email(email)
     except ValidationError:
         raise ValidationError('Invalid email address.')
 
-    username_validator(self, username)
+    username_validator(username)
 
-    user_password_validator(self, password, confirm_password)
+    user_password_validator(password, confirm_password)
 
-    if self.filter(email=email):
+    if User.objects.filter(email=email):
         raise ValueError('Email already exists.')
     return True
 
 
-def user_password_validator(self, password: str, confirm_password: str = "") -> bool:
+def user_password_validator(password: str, confirm_password: str = "") -> bool:
     """Validate the user password before saving it to the database."""
     if len(password) < 8:
         raise ValidationError('Password must be at least 8 characters long.')
@@ -28,30 +31,30 @@ def user_password_validator(self, password: str, confirm_password: str = "") -> 
     return True
 
 
-def user_login_validator(self, email: str, password: str) -> bool:
+def user_login_validator(email: str, password: str) -> bool:
     """Validate the user login data before authenticating the user."""
     password = make_password(password)
-    if not self.filter(email=email) or not self.filter(password=password):
+    if not User.objects.filter(email=email) or not User.objects.filter(password=password):
         raise ValidationError('Invalid email or password.')
     return True
 
 
-def user_update_validator(self, username: str, password: str, confirm_password: str) -> bool:
+def user_update_validator(username: str, password: str, confirm_password: str) -> bool:
     """Validate the user data before updating it in the database."""
     if len(username) < 3:
         raise ValidationError('Username must be at least 3 characters long.')
 
-    if self.filter(username=username):
+    if User.objects.filter(username=username).exists():
         raise ValueError('Username already exists.')
 
-    user_password_validator(self, password, confirm_password)
+    user_password_validator(password, confirm_password)
     return True
 
 
-def username_validator(self, username: str) -> bool:
+def username_validator(username: str) -> bool:
     """Validate the username before saving it to the database."""
     if len(username) < 3:
         raise ValidationError('Username must be at least 3 characters long.')
-    if self.filter(username=username):
+    if User.objects.filter(username=username).exists():
         raise ValueError('Username already exists.')
     return True
