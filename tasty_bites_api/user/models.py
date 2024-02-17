5431781eae12e730e845fb9341f1802d02e13191
Vladyslav Duran <justez4rtz@gmail.com>
2024-02-17 18:15:46 +0200

    fix(models): remove unnecessery methods from UserManager class and change inheritence class for User model itself

--- tasty_bites_api/user/models.py
+++ tasty_bites_api/user/models.py
@@ -1,81 +1,47 @@
 from django.db import models
-from django.contrib.auth.hashers import make_password
-from django.contrib.auth.base_user import (AbstractBaseUser,
-                                           BaseUserManager)
-from .validators import (user_creation_validator,
-                         user_update_validator)
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
 
 
 class UserModelManager(BaseUserManager):
-    """Custom User Manager to handle the user creation and authentication."""
-
-    def create(self, username: str, email: str, password: str, confirm_password: str) -> 'User':
-        """Overwrite the user creation method with hashing password before saving user to the database."""
-        email: str = self.normalize_email(email)
-
-        user_creation_validator(self, username, email, password, confirm_password)
-
-        password: str = make_password(password)
-
-        user: 'User' = self.model(
-            username=username,
-            email=email,
-            password=password
-        )
-        user.save()
+    def create_user(self, username, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
         return user
 
-    def create_superuser(self, username: str, email: str, password: str, confirm_password: str) -> 'User':
-        """Overwrite the create_superuser method with hashing password before saving user to the database."""
-        email: str = self.normalize_email(email)
+    def create_superuser(self, username, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
 
-        user_creation_validator(self, username, email, password, confirm_password)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
 
-        password: str = make_password(password)
-
-        user: 'User' = self.model(
-            username=username,
-            email=email,
-            password=password,
-            is_staff=True,
-        )
-        user.save()
-        return user
-
-    def update(self, **kwargs: dict) -> 'User':
-        """Overwrite the user update method with hashing password before saving user to the database."""
-        user_update_validator(self, kwargs)
-
-        kwargs['password']: str = make_password(kwargs['password'])
-
-        user: 'User' = self.model(
-            username=kwargs['username'],
-            password=kwargs['password']
-        )
-        user.save()
-        return user
-
-    def delete_by_email(self, email: str) -> 'User':
-        """Overwrite the user delete method"""
-        user: 'User' = self.get(email=email)
-        user.delete()
-        return user
+        return self.create_user(username, email, password, **extra_fields)
 
 
-class User(AbstractBaseUser):
-    username = models.CharField(max_length=100, unique=True)
-    email = models.EmailField(max_length=100, unique=True)
-    password = models.CharField(max_length=100)
+class User(AbstractUser):
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=150, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    is_stuff = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
 
-    REQUIRED_FIELDS = ['email', 'password']
-
     USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
 
     objects = UserModelManager()
+
+    class Meta:
+        verbose_name = 'custom_verbose_name'
+        ordering = ['-created_at']
 
     def __str__(self):
         return self.email
