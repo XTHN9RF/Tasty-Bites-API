47f4b2cb55599c25294df387370a81da61920666
Vladyslav Duran <justez4rtz@gmail.com>
2024-02-18 05:33:26 +0200

    feat(user): create user profile view that handles requests with argument or without

--- tasty_bites_api/user/urls.py
+++ tasty_bites_api/user/urls.py
@@ -3,6 +3,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 from .views import UserRegistrationView
 from .views import UserLoginView
+from .views import UserProfileView
 
 namespace = 'user'
 
@@ -10,4 +11,6 @@ urlpatterns = [
     path('/register/', UserRegistrationView.as_view(), name='register'),
     path('/login/', UserLoginView.as_view(), name='login'),
     path('/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('/profile/<str:username>/', UserProfileView.as_view(), name='user_profile'),
+    path('/profile/', UserProfileView.as_view(), name='your_profile'),
 ]
--- tasty_bites_api/user/views.py
+++ tasty_bites_api/user/views.py
@@ -1,14 +1,13 @@
+from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.permissions import AllowAny
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 from rest_framework import generics
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .models import User
 from .serializers import UserRegistrationSerializer
 from .serializers import UserLoginSerializer
+from .serializers import UserProfileSerializer
 
 
 class UserLoginView(TokenObtainPairView):
@@ -20,3 +19,17 @@ class UserRegistrationView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserRegistrationSerializer
     permission_classes = (AllowAny,)
+
+
+class UserProfileView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        requested_user = self.kwargs.get('username', None)
+
+        if requested_user is None:
+            return self.request.user
+
+        return User.objects.get(username=requested_user)
