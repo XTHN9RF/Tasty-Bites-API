b3e341216c8a510c294cd0228cc89823cd142877
Vladyslav Duran <justez4rtz@gmail.com>
2024-02-27 00:29:33 +0200

    refactor(common-constants): fix str presentation of default avatar for user

--- tasty_bites_api/common/constants.py
+++ tasty_bites_api/common/constants.py
@@ -1,5 +1,4 @@
 DEFAULT_USER_AVATAR_PATH = 'media/user_avatars/default_avatar.png'
-DEFAULT_USER_AVATAR = 'user_avatars/default_avatar.png'
+DEFAULT_USER_AVATAR = 'media/user_avatars/default_avatar.png'
 
 AVATARS_UPLOAD_FOLDER = 'user_avatars/'
-
