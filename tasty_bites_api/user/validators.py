1bc0b3b725703a5248defe3f24473ee33596f5ff
Vladyslav Duran <justez4rtz@gmail.com>
2024-02-26 17:17:02 +0200

    refactor(validators): change usage of numbers to constants and remove unnecessery lines of code

--- tasty_bites_api/user/validators.py
+++ tasty_bites_api/user/validators.py
@@ -4,6 +4,9 @@ from django.core.validators import validate_email
 
 User = get_user_model()
 
+MINIMUM_PASSWORD_LENGTH = 8
+MINIMUM_USERNAME_LENGTH = 3
+
 
 def user_creation_validator(username: str, email: str, password: str, confirm_password) -> bool:
     """Validate the user data before saving it to the database."""
@@ -23,8 +26,8 @@ def user_creation_validator(username: str, email: str, password: str, confirm_pa
 
 def user_password_validator(password: str, confirm_password: str = "") -> bool:
     """Validate the user password before saving it to the database."""
-    if len(password) < 8:
-        raise ValidationError('Password must be at least 8 characters long.')
+    if len(password) < MINIMUM_PASSWORD_LENGTH:
+        raise ValidationError(f'Password must be at least {MINIMUM_PASSWORD_LENGTH} characters long.')
     if password != confirm_password:
         raise ValidationError('Passwords do not match.')
     return True
@@ -32,20 +35,15 @@ def user_password_validator(password: str, confirm_password: str = "") -> bool:
 
 def user_update_validator(username: str, password: str, confirm_password: str) -> bool:
     """Validate the user data before updating it in the database."""
-    if len(username) < 3:
-        raise ValidationError('Username must be at least 3 characters long.')
-
-    if User.objects.filter(username=username).exists():
-        raise ValueError('Username already exists.')
-
+    username_validator(username)
     user_password_validator(password, confirm_password)
     return True
 
 
 def username_validator(username: str) -> bool:
     """Validate the username before saving it to the database."""
-    if len(username) < 3:
-        raise ValidationError('Username must be at least 3 characters long.')
+    if len(username) < MINIMUM_USERNAME_LENGTH:
+        raise ValidationError(f'Username must be at least {MINIMUM_USERNAME_LENGTH} characters long.')
     if User.objects.filter(username=username).exists():
         raise ValueError('Username already exists.')
     return True
