4d8886348f826cf9ba4a8870eb8f8706a7eb1624
Vladyslav Duran <justez4rtz@gmail.com>
2024-04-02 16:33:57 +0300

    fix(celery): change celery and redis versions for it to work

--- tasty_bites_api/tasty_bites_api/celery.py
+++ tasty_bites_api/tasty_bites_api/celery.py
@@ -1,7 +1,6 @@
-from __future__ import absolute_import, unicode_literals
-import os
 from celery import Celery
 from django.conf import settings
+import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tasty_bites_api.settings')
 
