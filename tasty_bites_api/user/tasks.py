0dd874a90122215a659b8bdb0fdf7d0df45a7d78
Vladyslav Duran <justez4rtz@gmail.com>
2024-04-03 18:48:57 +0300

    feat(user-celery): set up celery for usage with redis and create sending email taks

--- tasty_bites_api/user/tasks.py
+++ tasty_bites_api/user/tasks.py
@@ -1,2 +1,19 @@
 from celery import shared_task
+from django.core.mail import send_mail
+import os
+from dotenv import load_dotenv
 
+from user.models import User
+
+
+@shared_task
+def send_email_task(username: str, mail_subject: str, mail_message: str) -> None:
+    load_dotenv()
+
+    user = User.objects.get(username=username)
+
+    send_mail(mail_subject,
+              mail_message,
+              os.getenv('EMAIL_HOST_USER'),
+              [user.email],
+              fail_silently=False)
