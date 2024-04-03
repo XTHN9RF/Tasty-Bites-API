0d9c58412cc6de303d59349ce4ea6e344a6badcd
Vladyslav Duran <justez4rtz@gmail.com>
2024-04-03 18:51:04 +0300

    feat(user-views): add email activation view and refactore other views that could send emails such as Register and PasswordReset views

--- tasty_bites_api/user/urls.py
+++ tasty_bites_api/user/urls.py
@@ -6,15 +6,17 @@ from .views import UserLoginView
 from .views import UserProfileView
 from .views import UserUpdateView
 from .views import UserPasswordResetView
+from .views import UserActivationView
 
 app_name = 'user'
 
 urlpatterns = [
-    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('register/', UserRegistrationView.as_view({'post': 'create_user'}), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/<str:username>/', UserProfileView.as_view(), name='user_profile'),
     path('profile/', UserProfileView.as_view(), name='your_profile'),
     path('update/', UserUpdateView.as_view({'put': 'update_profile'}), name='update_profile'),
     path('reset-password/', UserPasswordResetView.as_view({'post': 'reset_password'}), name='reset_password'),
+    path('activate/<str:token>/', UserActivationView.as_view({'get': 'activate_user'}), name='activate_user'),
 ]
--- tasty_bites_api/user/views.py
+++ tasty_bites_api/user/views.py
@@ -1,14 +1,13 @@
-from django.core.mail import send_mail
-from rest_framework.views import APIView
+from django.urls import reverse
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.permissions import AllowAny
 from rest_framework import generics
+from rest_framework_simplejwt.exceptions import TokenError
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.decorators import action
-import os
-from dotenv import load_dotenv
 from .models import User
 from .serializers import UserRegistrationSerializer
 from .serializers import UserLoginSerializer
@@ -17,6 +16,8 @@ from .permissions import IsObjectOwnerOrReadOnly
 
 from common.utils import HttpMethods
 
+from user.tasks import send_email_task
+
 
 class UserLoginView(TokenObtainPairView):
     """A view that allows user to login into website and get both access and refresh tokens."""
@@ -24,11 +25,52 @@ class UserLoginView(TokenObtainPairView):
     serializer_class = UserLoginSerializer
 
 
-class UserRegistrationView(generics.CreateAPIView):
+class UserRegistrationView(generics.CreateAPIView, ViewSet):
     """A view that allows user to register into website, providing username, email and password."""
     queryset = User.objects.all()
     serializer_class = UserRegistrationSerializer
     permission_classes = (AllowAny,)
+
+    @action(detail=False, methods=HttpMethods.POST.value)
+    def create_user(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            user = serializer.create(serializer.validated_data)
+
+            token = RefreshToken.for_user(user)
+
+            relative_link = reverse('user:activate_user', kwargs={'token': str(token)})
+
+            current_link = request.scheme + '://' + request.get_host()
+
+            mail_subject = 'Account Activation'
+            mail_message = (f'Please click the following link to activate your account:\n' +
+                            f'{current_link}{relative_link}')
+
+            send_email_task.delay(user.username, mail_subject, mail_message)
+
+            return Response(
+                {'message': 'User created successfully. Check your email for account activation.'}, status=201)
+
+        return Response(serializer.errors, status=400)
+
+
+class UserActivationView(ViewSet):
+    """A view that allows user to activate their account by clicking on the link sent to their email."""
+    permission_classes = (AllowAny,)
+
+    @action(detail=True, methods=HttpMethods.GET.value)
+    def activate_user(self, request, token):
+        try:
+            token = RefreshToken(token)
+            user = User.objects.get(id=token.payload['user_id'])
+            user.is_active = True
+            user.save()
+            return Response({'message': 'User activated successfully.'}, status=200)
+        except TokenError:
+            return Response({'message': 'Invalid or expired token'}, status=400)
+        except User.DoesNotExist:
+            return Response({'message': 'User does not exist'}, status=400)
 
 
 class UserProfileView(generics.RetrieveAPIView):
@@ -74,18 +116,14 @@ class UserPasswordResetView(ViewSet):
 
     @action(detail=False, methods=HttpMethods.POST.value)
     def reset_password(self, request):
-        load_dotenv()
         user = User.objects.get(username=self.request.user.username)
         new_password = User.objects.make_random_password()
         user.set_password(new_password)
         user.save()
 
-        send_mail(
-            'Password Reset',
-            f'Your new password is: {new_password}',
-            os.getenv('EMAIL_HOST_USER'),
-            [user.email],
-            fail_silently=False
-        )
+        mail_subject = 'Password Reset'
+        mail_message = f'Your new password is: {new_password}'
+
+        send_email_task.delay(user.username, mail_subject, mail_message)
 
         return Response({'message': 'Password reset successfully. Check your email for new password.'}, status=200)
